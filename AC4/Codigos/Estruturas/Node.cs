137549cb6083b35bdce4ee666d71509cd1be984d
Matheus Freitas <freitas.matheus.profissional@outlook.com>
2022-12-23 15:58:25 -0300

    Resolução da questão 2

--- AC4/Codigos/Estruturas/Node.cs
+++ AC4/Codigos/Estruturas/Node.cs
@@ -2,18 +2,18 @@ namespace Codigos.Estruturas;
 
 public class Node
 {
-    public int Key { get; set; }
-    public Object Obj { get; set; }
-    public Node Left { get; set; }
-    public Node Right { get; set; }
-    public Node(int key, Object obj)
+    public int Key { get; set; } // Chave que servirá para organizar a tree
+    public Object Obj { get; set; } // Conteúdo que será guardado nesse node
+    public Node Left { get; set; } // Node à esquerda
+    public Node Right { get; set; } // Node à direita
+    public Node(int key, Object obj) // Construtor do Node
     {
         Key = key;
         Obj = obj;
         Left = null;
         Right = null;
     }
-    public override string ToString()
+    public override string ToString() // Método para mostrar melhor o node
     {
         return $"Id: {Key}, Conteúdo: {Obj}";
     }
